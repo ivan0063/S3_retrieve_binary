bbe6bec1e6c52e76a7466a85923ead60b97ce6a3
J. Ivan Martinez Mateos <jimm0063@gmail.com>
2021-04-23 10:37:00 -0500

    first commit

---
+++ src/test/java/com/ivan0063/test/aws/s3binaryretrieve/S3BinaryRetrieveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivan0063.test.aws.s3binaryretrieve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3BinaryRetrieveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
